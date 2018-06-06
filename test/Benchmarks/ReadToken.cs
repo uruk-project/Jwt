233f3b922569645c2528512b2209bc6330bb5a66
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2018-06-06 22:43:24 +0200

    Benchmark update

--- test/Benchmarks/ReadToken.cs
+++ test/Benchmarks/ReadToken.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Jose;
 using JWT;
 using JWT.Algorithms;
 using JWT.Serializers;
@@ -12,15 +13,13 @@ namespace JsonWebToken.Performance
     [Config(typeof(DefaultCoreConfig))]
     public class ReadToken
     {
-        private static readonly IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
         private static readonly IJsonSerializer serializer = new JsonNetSerializer();
         private static readonly IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
         private static readonly IDateTimeProvider dateTimeProvider = new UtcDateTimeProvider();
-        public static readonly IJwtEncoder JwtDotNetEncoder = new JwtEncoder(algorithm, serializer, urlEncoder);
         public static readonly JwtDecoder JwtDotNetDecoder = new JwtDecoder(serializer, new JwtValidator(serializer, dateTimeProvider), urlEncoder);
 
         private static readonly SymmetricJwk SymmetricKey = Tokens.SigningKey;
-        public static readonly JsonWebTokenReader Reader = new JsonWebTokenReader();
+        public static readonly JsonWebTokenReader Reader = new JsonWebTokenReader(Tokens.EncryptionKey);
         private static readonly TokenValidationParameters validationParameters = TokenValidationParameters.NoValidation;
         public static readonly JwtSecurityTokenHandler Handler = new JwtSecurityTokenHandler() { MaximumTokenSizeInBytes = 4 * 1024 * 1024 };
 
@@ -31,53 +30,64 @@ namespace JsonWebToken.Performance
             var result = Reader.TryReadToken(Tokens.ValidTokens[token].AsSpan(), validationParameters);
             if (!result.Succedeed)
             {
-                throw new Exception();
+                throw new Exception(result.Status.ToString());
             }
         }
 
-        [Benchmark]
-        [ArgumentsSource(nameof(GetTokens))]
-        public void Wilson(string token)
-        {
-            var result = Handler.ReadJwtToken(Tokens.ValidTokens[token]);
-            if (result == null)
-            {
-                throw new Exception();
-            }
-        }
+        //[Benchmark]
+        //[ArgumentsSource(nameof(GetTokens))]
+        //public void Wilson(string token)
+        //{
+        //    var result = Handler.ReadJwtToken(Tokens.ValidTokens[token]);
+        //    if (result == null)
+        //    {
+        //        throw new Exception();
+        //    }
+        //}
 
-        [Benchmark]
-        [ArgumentsSource(nameof(GetTokens))]
-        public void JoseDotNet(string token)
-        {
-            //unable to read the token without signature validation
-            var value = Jose.JWT.Decode<Dictionary<string, object>>(Tokens.ValidTokens[token], SymmetricKey.RawK);
-            if (value == null)
-            {
-                throw new Exception();
-            }
-        }
+        //[Benchmark]
+        //[ArgumentsSource(nameof(GetTokens))]
+        //public void JoseDotNet(string token)
+        //{
+        //    if (token.StartsWith("enc-"))
+        //    {
+        //        var jwt = Jose.JWT.Decode(Tokens.ValidTokens[token], key: Tokens.EncryptionKey.RawK, enc: JweEncryption.A128CBC_HS256, alg: JweAlgorithm.A128KW);
+        //        var value = Jose.JWT.Decode<Dictionary<string, object>>(jwt, key: Tokens.SigningKey.RawK, alg: JwsAlgorithm.HS256);
+        //        if (value == null)
+        //        {
+        //            throw new Exception();
+        //        }
+        //    }
+        //    else
+        //    {
+        //        var value = Jose.JWT.Decode<Dictionary<string, object>>(Tokens.ValidTokens[token], key: Tokens.SigningKey.RawK, alg: JwsAlgorithm.HS256);
+        //        if (value == null)
+        //        {
+        //            throw new Exception();
+        //        }
+        //    }
+        //}
 
-        [Benchmark]
-        [ArgumentsSource(nameof(GetNotEncryptedTokens))]
-        public void JwtDotNet(string token)
-        {
-            var value = JwtDotNetDecoder.DecodeToObject(Tokens.ValidTokens[token]);
-            if (value == null)
-            {
-                throw new Exception();
-            }
-        }
+        //[Benchmark]
+        //[ArgumentsSource(nameof(GetNotEncryptedTokens))]
+        //public void JwtDotNet(string token)
+        //{
+        //    var value = JwtDotNetDecoder.DecodeToObject(Tokens.ValidTokens[token]);
+        //    if (value == null)
+        //    {
+        //        throw new Exception();
+        //    }
+        //}
         public IEnumerable<object[]> GetTokens()
         {
-            yield return new[] { "empty" };
-            yield return new[] { "small" };
+            //yield return new[] { "empty" };
+            //yield return new[] { "small" };
             yield return new[] { "medium" };
-            yield return new[] { "big" };
-            yield return new[] { "enc-empty" };
-            yield return new[] { "enc-small" };
-            yield return new[] { "enc-medium" };
-            yield return new[] { "enc-big" };
+            //yield return new[] { "big" };
+            //yield return new[] { "enc-empty" };
+            //yield return new[] { "enc-small" };
+            //yield return new[] { "enc-medium" };
+            //yield return new[] { "enc-big" };
         }
 
         public IEnumerable<object[]> GetNotEncryptedTokens()
