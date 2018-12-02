437c1542681cc228f49579672acc2d2b8cc30531
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2018-12-02 21:15:11 +0100

    Add State descriptor

---
+++ src/OAuth2/EncryptedStatedDescriptor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2018 Yann Crumeyrolle. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace JsonWebToken
+{
+    public class EncryptedStatedDescriptor : JweDescriptor<StateDescriptor>
+    {
+        public EncryptedStatedDescriptor()
+        {
+        }
+
+        public EncryptedStatedDescriptor(StateDescriptor payload) : base(payload)
+        {
+        }
+
+        public EncryptedStatedDescriptor(IDictionary<string, object> header, StateDescriptor payload) : base(header, payload)
+        {
+        }
+    }
+}
