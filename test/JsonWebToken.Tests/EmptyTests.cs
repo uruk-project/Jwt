60316602a9e1f677e1703054084ab05a5acaaea1
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2018-12-28 00:36:11 +0100

    hotfix

--- test/JsonWebToken.Tests/EmptyTests.cs
+++ test/JsonWebToken.Tests/EmptyTests.cs
@@ -14,7 +14,7 @@ namespace JsonWebToken.Tests
         [Fact]
         public void Stackalloc()
         {
-            Span<byte> data = stackalloc byte[1024 * 1024];
+            Span<byte> data = stackalloc byte[1024 * 1024 /2];
             Assert.True(true);
         }
     }
