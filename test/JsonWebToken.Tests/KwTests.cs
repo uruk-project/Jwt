81a5ade96a1a2097ee062052f1951d8f0be8afe3
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2018-12-27 22:56:42 +0100

    Enable 5 UT

--- test/JsonWebToken.Tests/KwTests.cs
+++ test/JsonWebToken.Tests/KwTests.cs
@@ -1,26 +1,26 @@
-﻿//using Xunit;
-//using System;
-//using JsonWebToken.Internal;
+﻿using Xunit;
+using System;
+using JsonWebToken.Internal;
 
-//namespace JsonWebToken.Tests
-//{
-//    public class KwTests
-//    {
-//        private readonly SymmetricJwk _key = SymmetricJwk.FromBase64Url("U1oK6e4BAR4kKTdyA1OqEFYwX9pIrswuUMNt8qW4z-k");
-//        private readonly SymmetricJwk _keyToWrap = SymmetricJwk.FromBase64Url("gXoKEcss-xFuZceE6B3VkEMLw-f0h9tGfyaheF5jqP8");
-        
-//        [Fact]
-//        public void WrapUnwrap()
-//        {
-//            var kwp = new AesKeyWrapper(_key, EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.Aes256KW);
-//            byte[] wrappedKey = new byte[kwp.GetKeyWrapSize()];
+namespace JsonWebToken.Tests
+{
+    public class KwTests
+    {
+        private readonly SymmetricJwk _key = SymmetricJwk.FromBase64Url("U1oK6e4BAR4kKTdyA1OqEFYwX9pIrswuUMNt8qW4z-k");
+        private readonly SymmetricJwk _keyToWrap = SymmetricJwk.FromBase64Url("gXoKEcss-xFuZceE6B3VkEMLw-f0h9tGfyaheF5jqP8");
 
-//            var wrapped = kwp.TryWrapKey(_keyToWrap, null, wrappedKey, out var cek, out var bytesWritten);
-//            Assert.True(wrapped);
+        [Fact]
+        public void WrapUnwrap()
+        {
+            var kwp = new AesKeyWrapper(_key, EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.Aes256KW);
+            byte[] wrappedKey = new byte[kwp.GetKeyWrapSize()];
 
-//            var unwrappedKey = new byte[kwp.GetKeyUnwrapSize(wrappedKey.Length)];
-//            var unwrapped = kwp.TryUnwrapKey(wrappedKey, unwrappedKey, null, out int keyWrappedBytesWritten);
-//            Assert.True(unwrapped);
-//        }
-//    }
-//}
+            var wrapped = kwp.TryWrapKey(_keyToWrap, null, wrappedKey, out var cek, out var bytesWritten);
+            Assert.True(wrapped);
+
+            var unwrappedKey = new byte[kwp.GetKeyUnwrapSize(wrappedKey.Length)];
+            var unwrapped = kwp.TryUnwrapKey(wrappedKey, unwrappedKey, null, out int keyWrappedBytesWritten);
+            Assert.True(unwrapped);
+        }
+    }
+}
