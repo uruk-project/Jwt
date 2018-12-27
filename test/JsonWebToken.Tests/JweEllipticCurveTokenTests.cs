91f2358208d693dee731ea61b56a81e4a3dafbda
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2018-12-27 22:48:13 +0100

    Enable JweEllipticCurveTokenTests

--- test/JsonWebToken.Tests/JweEllipticCurveTokenTests.cs
+++ test/JsonWebToken.Tests/JweEllipticCurveTokenTests.cs
@@ -1,59 +1,59 @@
-﻿//using System.Collections.Generic;
-//using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
-//namespace JsonWebToken.Tests
-//{
-//    public class JweEllipticCurveTokenTests
-//    {
-//        private readonly ECJwk _bobKey = new ECJwk
-//        {
-//            Kty = "EC",
-//            Crv = "P-256",
-//            X = "weNJy2HscCSM6AEDTDg04biOvhFhyyWvOHQfeF_PxMQ",
-//            Y = "e8lnCO-AlStT-NJVX-crhB7QRYhiix03illJOVAOyck",
-//            D = "VEmDZpDXXK8p8N0Cndsxs924q6nS1RXFASRl6BfUqdw",
-//        };
-//        private readonly SymmetricJwk _signingKey = SymmetricJwk.GenerateKey(256, SignatureAlgorithm.HmacSha256);
+namespace JsonWebToken.Tests
+{
+    public class JweEllipticCurveTokenTests
+    {
+        private readonly ECJwk _bobKey = new ECJwk
+        {
+            Kty = "EC",
+            Crv = "P-256",
+            X = "weNJy2HscCSM6AEDTDg04biOvhFhyyWvOHQfeF_PxMQ",
+            Y = "e8lnCO-AlStT-NJVX-crhB7QRYhiix03illJOVAOyck",
+            D = "VEmDZpDXXK8p8N0Cndsxs924q6nS1RXFASRl6BfUqdw",
+        };
+        private readonly SymmetricJwk _signingKey = SymmetricJwk.GenerateKey(256, SignatureAlgorithm.HmacSha256);
 
-//        [Theory]
-//        [MemberData(nameof(GetSupportedAlgorithm))]
-//        public void Encode_Decode(string enc, string alg)
-//        {
-//            var writer = new JwtWriter();
+        [Theory]
+        [MemberData(nameof(GetSupportedAlgorithm))]
+        public void Encode_Decode(string enc, string alg)
+        {
+            var writer = new JwtWriter();
 
-//            var descriptor = new JweDescriptor
-//            {
-//                Key = _bobKey,
-//                EncryptionAlgorithm = (EncryptionAlgorithm)enc,
-//                Algorithm = alg,
-//                Payload = new JwsDescriptor
-//                {
-//                    Key = _signingKey,
-//                    Algorithm = SignatureAlgorithm.HmacSha256.Name,
-//                    Subject = "Alice"
-//                }
-//            };
+            var descriptor = new JweDescriptor
+            {
+                Key = _bobKey,
+                EncryptionAlgorithm = (EncryptionAlgorithm)enc,
+                Algorithm = alg,
+                Payload = new JwsDescriptor
+                {
+                    Key = _signingKey,
+                    Algorithm = SignatureAlgorithm.HmacSha256.Name,
+                    Subject = "Alice"
+                }
+            };
 
-//            var token = writer.WriteToken(descriptor);
+            var token = writer.WriteToken(descriptor);
 
-//            var reader = new JwtReader(_bobKey);
-//            var policy = new TokenValidationPolicyBuilder()
-//                .RequireSignature(_signingKey)
-//                    .Build();
+            var reader = new JwtReader(_bobKey);
+            var policy = new TokenValidationPolicyBuilder()
+                .RequireSignature(_signingKey)
+                    .Build();
 
-//            var result = reader.TryReadToken(token, policy);
-//            Assert.Equal(TokenValidationStatus.Success, result.Status);
-//            Assert.Equal("Alice", result.Token.Subject);
-//        }
+            var result = reader.TryReadToken(token, policy);
+            Assert.Equal(TokenValidationStatus.Success, result.Status);
+            Assert.Equal("Alice", result.Token.Subject);
+        }
 
-//        public static IEnumerable<object[]> GetSupportedAlgorithm()
-//        {
-//            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEs };
-//            yield return new object[] { EncryptionAlgorithm.Aes192CbcHmacSha384, KeyManagementAlgorithm.EcdhEs };
-//            yield return new object[] { EncryptionAlgorithm.Aes256CbcHmacSha512, KeyManagementAlgorithm.EcdhEs };
-//            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes128KW };
-//            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes192KW };
-//            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes256KW };
-//        }
-//    }
-//}
+        public static IEnumerable<object[]> GetSupportedAlgorithm()
+        {
+            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEs };
+            yield return new object[] { EncryptionAlgorithm.Aes192CbcHmacSha384, KeyManagementAlgorithm.EcdhEs };
+            yield return new object[] { EncryptionAlgorithm.Aes256CbcHmacSha512, KeyManagementAlgorithm.EcdhEs };
+            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes128KW };
+            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes192KW };
+            yield return new object[] { EncryptionAlgorithm.Aes128CbcHmacSha256, KeyManagementAlgorithm.EcdhEsAes256KW };
+        }
+    }
+}
