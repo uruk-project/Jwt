dfeb3c399c51a72106fdebae21cb539d2710f6ab
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2019-01-03 13:35:05 +0100

    fix build

--- perf/Benchmarks/JweBenchmark.cs
+++ perf/Benchmarks/JweBenchmark.cs
@@ -13,14 +13,14 @@ namespace JsonWebToken.Performance
             yield return "JWE-big";
         }
 
-        [Benchmark(OperationsPerInvoke = IterationCount)]
+        [Benchmark]
         [ArgumentsSource(nameof(GetTokens))]
         public override void ValidateJwt(string token)
         {
             ValidateJwtCore(token, policyWithSignatureValidation);
         }
 
-        [Benchmark(OperationsPerInvoke = IterationCount)]
+        [Benchmark]
         [ArgumentsSource(nameof(GetTokens))]
         public override void WriteJwt(string token)
         {
