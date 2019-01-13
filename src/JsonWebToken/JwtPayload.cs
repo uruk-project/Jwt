e1389ef2e1c2857f3beec8dd0f56293748c6d2ce
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2019-01-13 23:15:30 +0100

    change the JwtPayload also

--- src/JsonWebToken/JwtPayload.cs
+++ src/JsonWebToken/JwtPayload.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See the LICENSE file in the project root for more information.
 
 using JsonWebToken.Internal;
-using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 namespace JsonWebToken
@@ -10,14 +9,16 @@ namespace JsonWebToken
     /// <summary>
     /// Represents the claims contained in the JWT.
     /// </summary>
-    public sealed class JwtPayload : Dictionary<string, object>
+    public sealed class JwtPayload
     {
+        private readonly Dictionary<string, object> _inner;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="JwtPayload"/> class.
         /// </summary>
         public JwtPayload()
-            : base()
         {
+            _inner = new Dictionary<string, object>();
         }
 
         /// <summary>
@@ -25,8 +26,8 @@ namespace JsonWebToken
         /// </summary>
         /// <param name="inner"></param>
         public JwtPayload(Dictionary<string, object> inner)
-            : base(inner)
         {
+            _inner = inner;
         }
 
         /// <summary>
@@ -55,11 +56,42 @@ namespace JsonWebToken
                     case Claims.Iat:
                         return Iat;
                     default:
-                        return TryGetValue(key, out var value) ? value : null;
+                        return _inner.TryGetValue(key, out var value) ? value : null;
                 }
             }
 
-            set => base[key] = value;
+            set
+            {
+                switch (key)
+                {
+                    case Claims.Iss:
+                        Iss = (string)value;
+                        break;
+                    case Claims.Exp:
+                        Exp = (long?)value;
+                        break;
+                    case Claims.Aud:
+                        Aud = (IList<string>)value;
+                        break;
+                    case Claims.Jti:
+                        Jti = (string)value;
+                        break;
+                    case Claims.Sub:
+                        Sub = (string)value;
+                        break;
+                    case Claims.Nbf:
+                        Nbf = (long?)value;
+                        break;
+                    case Claims.Iat:
+                        Iat = (long?)value;
+                        break;
+                    default:
+                        _inner[key] = value;
+                        break;
+                }
+
+                _inner[key] = value;
+            }
         }
 
         /// <summary>
@@ -105,12 +137,30 @@ namespace JsonWebToken
         /// <returns></returns>
         public T GetValue<T>(string key)
         {
-            if (TryGetValue(key, out var value) && value is T tValue)
+            if (_inner.TryGetValue(key, out var value) && value is T tValue)
             {
                 return tValue;
             }
 
             return default;
+        }
+
+        /// <summary>
+        /// Determines whether the <see cref="JwtPayload"/> contains the specified key.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey(string key)
+        {
+            return _inner.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified key.
+        /// </summary>
+        public bool TryGetValue(string key, out object value)
+        {
+            return _inner.TryGetValue(key, out value);
         }
     }
 }
