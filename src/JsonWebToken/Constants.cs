548f0d15b92ec5d6ba5db62b02a710bbe8657cdd
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2019-07-10 09:39:52 +0200

    JsonWriterOptions is used by copy instead of new allocation

--- src/JsonWebToken/Constants.cs
+++ src/JsonWebToken/Constants.cs
@@ -1,6 +1,8 @@
 // Copyright (c) 2018 Yann Crumeyrolle. All rights reserved.
 // Licensed under the MIT license. See the LICENSE file in the project root for more information.
 
+using System.Text.Json;
+
 namespace JsonWebToken
 {
     internal static class Constants
@@ -14,5 +16,7 @@ namespace JsonWebToken
         internal static readonly int DecompressionBufferLength = 1024;
 
         internal const byte ByteDot = (byte)'.';
+
+        internal static readonly JsonWriterOptions NoJsonValidation = new JsonWriterOptions { SkipValidation = true };
     }
 }
