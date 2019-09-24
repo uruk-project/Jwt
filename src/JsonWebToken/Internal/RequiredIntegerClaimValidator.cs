0814f3ba338c2c797b29e55c5fd916a058b006da
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2019-09-24 23:25:06 +0200

    Nits

---
+++ src/JsonWebToken/Internal/RequiredIntegerClaimValidator.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2018 Yann Crumeyrolle. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace JsonWebToken.Internal
+{
+    /// <summary>
+    /// Represents a <see cref="IValidator"/> verifying the JWT has a required claim.
+    /// </summary>
+    internal class RequiredIntegerClaimValidator : IValidator
+    {
+        private readonly string _claim;
+        private readonly long? _value;
+
+        /// <summary>
+        /// Initializes an instance of <see cref="RequiredIntegerClaimValidator"/>.
+        /// </summary>
+        /// <param name="claim"></param>
+        public RequiredIntegerClaimValidator(string claim)
+            : this(claim, null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="RequiredIntegerClaimValidator"/>.
+        /// </summary>
+        /// <param name="claim"></param>
+        /// <param name="value"></param>
+        public RequiredIntegerClaimValidator(string claim, long? value)
+        {
+            if (claim is null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.claim);
+            }
+
+            _claim = claim;
+            _value = value;
+        }
+
+        /// <inheritdoc />
+        public TokenValidationResult TryValidate(Jwt jwt)
+        {
+            if (jwt is null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.jwt);
+            }
+
+            if (jwt.Payload is null)
+            {
+                return TokenValidationResult.MalformedToken();
+            }
+
+            var claim = jwt.Payload[_claim];
+            if (claim is null)
+            {
+                return TokenValidationResult.MissingClaim(jwt, _claim);
+            }
+
+            if (_value != (long?)claim)
+            {
+                return TokenValidationResult.InvalidClaim(jwt, _claim);
+            }
+
+            return TokenValidationResult.Success(jwt);
+        }
+    }
+}
---
+++ src/JsonWebToken/Internal/RequiredStringClaimValidator.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) 2018 Yann Crumeyrolle. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for more information.
+
+namespace JsonWebToken.Internal
+{
+    /// <summary>
+    /// Represents a <see cref="IValidator"/> verifying the JWT has a required claim.
+    /// </summary>
+    internal class RequiredStringClaimValidator : IValidator
+    {
+        private readonly string _claim;
+        private readonly string _value;
+
+        /// <summary>
+        /// Initializes an instance of <see cref="RequiredStringClaimValidator"/>.
+        /// </summary>
+        /// <param name="claim"></param>
+        /// <param name="value"></param>
+        public RequiredStringClaimValidator(string claim, string value)
+        {
+            if (claim is null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.claim);
+            }
+
+            if (value is null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.value);
+            }
+
+            _claim = claim;
+            _value = value;
+        }
+
+        /// <inheritdoc />
+        public TokenValidationResult TryValidate(Jwt jwt)
+        {
+            if (jwt is null)
+            {
+                ThrowHelper.ThrowArgumentNullException(ExceptionArgument.jwt);
+            }
+
+            if (jwt.Payload is null)
+            {
+                return TokenValidationResult.MalformedToken();
+            }
+
+            var claim = jwt.Payload[_claim];
+            if (claim is null)
+            {
+                return TokenValidationResult.MissingClaim(jwt, _claim);
+            }
+
+            if (!string.Equals(_value, (string?)claim))
+            {
+                return TokenValidationResult.InvalidClaim(jwt, _claim);
+            }
+
+            return TokenValidationResult.Success(jwt);
+        }
+    }
+}
