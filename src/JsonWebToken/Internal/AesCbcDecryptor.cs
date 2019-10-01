cffbc7e49cb9441043c9bc6409c0af290edc645e
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2019-10-01 18:26:32 +0200

    Extract duplicate code

--- src/JsonWebToken/Internal/AesCbcDecryptor.cs
+++ src/JsonWebToken/Internal/AesCbcDecryptor.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See the LICENSE file in the project root for more information.
 
 using System;
-using System.Buffers;
 using System.Security.Cryptography;
 
 namespace JsonWebToken.Internal
@@ -59,7 +58,7 @@ namespace JsonWebToken.Internal
                 aes.IV = nonce.ToArray();
                 using (var decryptor = aes.CreateDecryptor())
                 {
-                    bytesWritten = Transform(decryptor, ciphertext, 0, ciphertext.Length, plaintext);
+                    bytesWritten = AesCbcHelper.Transform(decryptor, ciphertext, 0, ciphertext.Length, plaintext);
                 }
 
                 return bytesWritten <= ciphertext.Length;
@@ -78,87 +77,6 @@ namespace JsonWebToken.Internal
                 _aesPool.Dispose();
                 _disposed = true;
             }
-        }
-
-        private static int Transform(ICryptoTransform transform, ReadOnlySpan<byte> input, int inputOffset, int inputLength, Span<byte> output)
-        {
-            byte[] buffer = input.ToArray();
-            int offset = inputOffset;
-            int count = inputLength;
-            var _inputBlockSize = transform.InputBlockSize;
-            var _outputBlockSize = transform.OutputBlockSize;
-
-            var _inputBuffer = new byte[_inputBlockSize];
-            var _outputBuffer = new byte[_outputBlockSize];
-            int _inputBufferIndex = 0;
-
-            // write <= count bytes to the output stream, transforming as we go.
-            // Basic idea: using bytes in the _InputBuffer first, make whole blocks,
-            // transform them, and write them out.  Cache any remaining bytes in the _InputBuffer.
-            int bytesToWrite = count;
-            int currentInputIndex = offset;
-
-            // if we have some bytes in the _InputBuffer, we have to deal with those first,
-            // so let's try to make an entire block out of it
-            int numOutputBytes;
-            int outputLength = 0;
-            while (bytesToWrite > 0)
-            {
-                if (bytesToWrite >= _inputBlockSize)
-                {
-                    // We have at least an entire block's worth to transform
-                    int numWholeBlocks = bytesToWrite / _inputBlockSize;
-
-                    // If the transform will handle multiple blocks at once, do that
-                    if (transform.CanTransformMultipleBlocks && numWholeBlocks > 1)
-                    {
-                        int numWholeBlocksInBytes = numWholeBlocks * _inputBlockSize;
-
-                        // Use ArrayPool.Shared instead of CryptoPool because the array is passed out.
-                        byte[] tempOutputBuffer = ArrayPool<byte>.Shared.Rent(numWholeBlocks * _outputBlockSize);
-                        numOutputBytes = 0;
-
-                        try
-                        {
-                            numOutputBytes = transform.TransformBlock(buffer, currentInputIndex, numWholeBlocksInBytes, tempOutputBuffer, 0);
-
-                            tempOutputBuffer.AsSpan(0, numOutputBytes).CopyTo(output.Slice(outputLength));
-                            outputLength += numOutputBytes;
-
-                            currentInputIndex += numWholeBlocksInBytes;
-                            bytesToWrite -= numWholeBlocksInBytes;
-                        }
-                        finally
-                        {
-                            CryptographicOperations.ZeroMemory(new Span<byte>(tempOutputBuffer, 0, numOutputBytes));
-                            ArrayPool<byte>.Shared.Return(tempOutputBuffer);
-                        }
-                    }
-                    else
-                    {
-                        // do it the slow way
-                        numOutputBytes = transform.TransformBlock(buffer, currentInputIndex, _inputBlockSize, _outputBuffer, 0);
-
-                        _outputBuffer.AsSpan(0, numOutputBytes).CopyTo(output.Slice(outputLength));
-                        outputLength += numOutputBytes;
-
-                        currentInputIndex += _inputBlockSize;
-                        bytesToWrite -= _inputBlockSize;
-                    }
-                }
-                else
-                {
-                    // In this case, we don't have an entire block's worth left, so store it up in the
-                    // input buffer, which by now must be empty.
-                    Buffer.BlockCopy(buffer, currentInputIndex, _inputBuffer, 0, bytesToWrite);
-                    _inputBufferIndex += bytesToWrite;
-                    break;
-                }
-            }
-
-            byte[] finalBytes = transform.TransformFinalBlock(_inputBuffer, 0, _inputBufferIndex);
-            finalBytes.AsSpan(0, finalBytes.Length).CopyTo(output.Slice(outputLength));
-            return outputLength + finalBytes.Length;
         }
 
         private sealed class AesPooledPolicy : PooledObjectFactory<Aes>
