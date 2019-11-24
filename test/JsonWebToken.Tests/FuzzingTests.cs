22759cd98e075508324f341b7ef61f82929027bc
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2019-11-24 17:44:58 +0100

    Add some tests on issues detected by fuzzing

---
+++ test/JsonWebToken.Tests/FuzzingTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace JsonWebToken.Tests
+{
+    public class FuzzingTests
+    {
+        static Jwk key = SymmetricJwk.GenerateKey(256);
+        static JwtReader reader = new JwtReader(key);
+
+        [Theory]
+        [InlineData("åyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIwIiwibmFtZSI6IkplIiwiaWF0IjoyfQ.")]
+        [InlineData("eyJhbGciOiJIUzI1NbIsInR5cCI6CI6IkpXVCJ9.eyJzdWIiOiIwIiwib‚‚‚‚‚‚‚‚‚‚IiOmFiwiaW>0IIsInR5cCI6CI6IkpjoyfQ.")]
+        [InlineData("eyJhbGciOiJiUzI1NiIsInR5cCI6IopXVCJ9.eyJzdWIiOiIwIiwibmFtZSI6IkplIiwiaWF0IjoyfQ.")]
+        public void Fuzz(string value)
+        {
+            var result = reader.TryReadToken(value, TokenValidationPolicy.NoValidation);
+
+            Assert.NotNull(result);
+        }
+    }
+}
