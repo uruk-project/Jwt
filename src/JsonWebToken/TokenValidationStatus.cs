d1596b488894b4be80aa4e6ab5c8d0dd020280ca
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-02-02 12:27:29 +0100

    Remove [Flags] attribute

--- src/JsonWebToken/TokenValidationStatus.cs
+++ src/JsonWebToken/TokenValidationStatus.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright (c) 2020 Yann Crumeyrolle. All rights reserved.
 // Licensed under the MIT license. See LICENSE in the project root for license information.
 
-using System;
-
 namespace JsonWebToken
 {
     /// <summary>
     /// Defines the valitation status of a JWT.
     /// </summary>
-    [Flags]
     public enum TokenValidationStatus
     {
         /// <summary>
