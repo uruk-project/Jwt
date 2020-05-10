2b4f21557b60a20bddeae52780d5c7fe512f5632
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-05-10 19:20:34 +0200

    React to benchmarkdotnet update

--- perf/Benchmarks/AllTfmCoreConfig.cs
+++ perf/Benchmarks/AllTfmCoreConfig.cs
@@ -5,8 +5,6 @@ using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Exporters;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Toolchains.CsProj;
-using BenchmarkDotNet.Toolchains.DotNetCli;
 using BenchmarkDotNet.Validators;
 
 namespace JsonWebToken.Performance
@@ -15,26 +13,18 @@ namespace JsonWebToken.Performance
     {
         public AllTfmCoreConfig()
         {
-            Add(ConsoleLogger.Default);
-            Add(MarkdownExporter.GitHub);
-            Add(MemoryDiagnoser.Default);
-            Add(StatisticColumn.OperationsPerSecond);
-            Add(DefaultColumnProviders.Instance);
+            AddLogger(ConsoleLogger.Default);
+            AddExporter(MarkdownExporter.GitHub);
+            AddDiagnoser(MemoryDiagnoser.Default);
+            AddColumn(StatisticColumn.OperationsPerSecond);
+            AddColumnProvider(DefaultColumnProviders.Instance);
 
-            Add(JitOptimizationsValidator.FailOnError);
-            Add(BenchmarkLogicalGroupRule.ByCategory);
+            AddValidator(JitOptimizationsValidator.FailOnError);
+            AddLogicalGroupRules(BenchmarkLogicalGroupRule.ByCategory);
 
-            //Add(Job.Core
-            //    .With(CsProjCoreToolchain.From(NetCoreAppSettings.NetCoreApp20))
-            //    .With(new GcMode { Server = true }));
-
-            //Add(Job.Default
-            //    .With(CoreRuntime.Core21) 
-            //    .With(new GcMode { Server = true }));
-
-            Add(Job.Default
-                .With(CoreRuntime.Core31)
-                .With(new GcMode { Server = true }));
+            AddJob(Job.Default
+                .WithGcMode(new GcMode { Server = true })
+                .WithRuntime(CoreRuntime.Core31));
         }
     }
 }
