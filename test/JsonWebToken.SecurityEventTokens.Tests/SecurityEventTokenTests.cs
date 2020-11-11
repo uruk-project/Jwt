989fa05b6fdd5df41179d79c69c890202a13b60c
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-11-11 11:55:21 +0100

    Fix build & tests

--- test/JsonWebToken.SecurityEventTokens.Tests/SecurityEventTokenTests.cs
+++ test/JsonWebToken.SecurityEventTokens.Tests/SecurityEventTokenTests.cs
@@ -19,7 +19,7 @@ namespace JsonWebToken.Tests
         [Fact]
         public void Write_V2()
         {
-            var descriptor = new SecurityEventTokenDescriptor(Jwk.None, SignatureAlgorithm.None)
+            var descriptor = new SecEventDescriptor(Jwk.None, SignatureAlgorithm.None)
             {
                 Payload = new JwtPayload
                 {
@@ -84,8 +84,7 @@ namespace JsonWebToken.Tests
             var result = Jwt.TryParse("eyJ0eXAiOiJzZWNldmVudCtqd3QiLCJhbGciOiJub25lIn0.eyJqdGkiOiI0ZDM1NTllYzY3NTA0YWFiYTY1ZDQwYjAzNjNmYWFkOCIsImlhdCI6MTQ1ODQ5NjQwNCwiaXNzIjoiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tIiwiYXVkIjpbImh0dHBzOi8vc2NpbS5leGFtcGxlLmNvbS9GZWVkcy85OGQ1MjQ2MWZhNWJiYzg3OTU5M2I3NzU0IiwiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tL0ZlZWRzLzVkNzYwNDUxNmIxZDA4NjQxZDc2NzZlZTciXSwiZXZlbnRzIjp7InVybjppZXRmOnBhcmFtczpzY2ltOmV2ZW50OmNyZWF0ZSI6eyJyZWYiOiJodHRwczovL3NjaW0uZXhhbXBsZS5jb20vVXNlcnMvNDRmNjE0MmRmOTZiZDZhYjYxZTc1MjFkOSIsImF0dHJpYnV0ZXMiOlsiaWQiLCJuYW1lIiwidXNlck5hbWUiLCJwYXNzd29yZCIsImVtYWlscyJdfX19.", policy, out var jwt);
             Assert.True(result);
 
-            var token = jwt.AsSecurityEventToken();
-            var events = token.Events;
+            jwt.Payload.TryGetClaim(SecEventClaims.Events, out var events);
             Assert.Equal("https://scim.example.com/Users/44f6142df96bd6ab61e7521d9", events["urn:ietf:params:scim:event:create"]["ref"].GetString());
             Assert.True(events.ContainsKey("urn:ietf:params:scim:event:create"));
             Assert.True(events["urn:ietf:params:scim:event:create"].ContainsKey("ref"));
