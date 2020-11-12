300de5e01fef107b280d33860e25a8288bc3239c
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-11-12 15:31:29 +0100

    Split secevent tests

--- test/JsonWebToken.SecurityEventTokens.Tests/SecEventDescriptorTests.cs
+++ test/JsonWebToken.SecurityEventTokens.Tests/SecEventDescriptorTests.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Buffers;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 using Xunit;
 
 namespace JsonWebToken.Tests
 {
-    public class SecurityEventTokenTests : IClassFixture<KeyFixture>
+    public class SecEventDescriptorTests : IClassFixture<KeyFixture>
     {
-        private readonly KeyFixture _keys;
-
-        public SecurityEventTokenTests(KeyFixture keys)
-        {
-            _keys = keys;
-        }
-
         [Fact]
-        public void Write_V2()
+        public void Write_Success()
         {
             var descriptor = new SecEventDescriptor(Jwk.None, SignatureAlgorithm.None)
             {
@@ -60,60 +50,6 @@ namespace JsonWebToken.Tests
 
             [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore, NullValueHandling = NullValueHandling.Ignore, PropertyName = "attributes", Required = Required.Default)]
             public IList<string> Attributes => _attributes;
-        }
-
-        [Fact]
-        public void Read()
-        {
-            var policy = new TokenValidationPolicyBuilder()
-                                .AcceptUnsecureTokenByDefault()
-                                .RequireSecEventToken()
-                                .WithDecryptionKeys(_keys.Jwks)
-                                .Build();
-            var result = Jwt.TryParse("eyJ0eXAiOiJzZWNldmVudCtqd3QiLCJhbGciOiJub25lIn0.eyJqdGkiOiI0ZDM1NTllYzY3NTA0YWFiYTY1ZDQwYjAzNjNmYWFkOCIsImlhdCI6MTQ1ODQ5NjQwNCwiaXNzIjoiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tIiwiYXVkIjpbImh0dHBzOi8vc2NpbS5leGFtcGxlLmNvbS9GZWVkcy85OGQ1MjQ2MWZhNWJiYzg3OTU5M2I3NzU0IiwiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tL0ZlZWRzLzVkNzYwNDUxNmIxZDA4NjQxZDc2NzZlZTciXSwiZXZlbnRzIjp7InVybjppZXRmOnBhcmFtczpzY2ltOmV2ZW50OmNyZWF0ZSI6eyJyZWYiOiJodHRwczovL3NjaW0uZXhhbXBsZS5jb20vVXNlcnMvNDRmNjE0MmRmOTZiZDZhYjYxZTc1MjFkOSIsImF0dHJpYnV0ZXMiOlsiaWQiLCJuYW1lIiwidXNlck5hbWUiLCJwYXNzd29yZCIsImVtYWlscyJdfX19.", policy, out var jwt);
-            Assert.True(result);
-
-            jwt.Payload.TryGetClaim(SecEventClaims.Events, out var events);
-            Assert.Equal("https://scim.example.com/Users/44f6142df96bd6ab61e7521d9", events["urn:ietf:params:scim:event:create"]["ref"].GetString());
-            Assert.True(events.ContainsKey("urn:ietf:params:scim:event:create"));
-            Assert.True(events["urn:ietf:params:scim:event:create"].ContainsKey("ref"));
-            Assert.False(events.ContainsKey("X"));
-            Assert.False(events["urn:ietf:params:scim:event:create"].ContainsKey("Y"));
-        }
-
-        internal class FixedSizedBufferWriter : IBufferWriter<byte>
-        {
-            private readonly byte[] _buffer;
-            private int _count;
-
-            public FixedSizedBufferWriter(int capacity)
-            {
-                _buffer = new byte[capacity];
-            }
-
-            public void Clear()
-            {
-                _count = 0;
-            }
-
-            public Span<byte> Free => _buffer.AsSpan(_count);
-
-            public byte[] Formatted => _buffer.AsSpan(0, _count).ToArray();
-
-            public Memory<byte> GetMemory(int minimumLength = 0) => _buffer.AsMemory(_count);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public Span<byte> GetSpan(int minimumLength = 0) => _buffer.AsSpan(_count);
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public void Advance(int bytes)
-            {
-                _count += bytes;
-                if (_count > _buffer.Length)
-                {
-                    throw new InvalidOperationException("Cannot advance past the end of the buffer.");
-                }
-            }
         }
     }
 }
---
+++ test/JsonWebToken.SecurityEventTokens.Tests/SecEventValidationTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace JsonWebToken.Tests
+{
+    public class SecEventValidationTests : IClassFixture<KeyFixture>
+    {
+        private readonly KeyFixture _keys;
+
+        public SecEventValidationTests(KeyFixture keys)
+        {
+            _keys = keys;
+        }
+
+        [Fact]
+        public void Read()
+        {
+            var policy = new TokenValidationPolicyBuilder()
+                                .AcceptUnsecureTokenByDefault()
+                                .RequireSecEventToken()
+                                .WithDecryptionKeys(_keys.Jwks)
+                                .Build();
+            var result = Jwt.TryParse("eyJ0eXAiOiJzZWNldmVudCtqd3QiLCJhbGciOiJub25lIn0.eyJqdGkiOiI0ZDM1NTllYzY3NTA0YWFiYTY1ZDQwYjAzNjNmYWFkOCIsImlhdCI6MTQ1ODQ5NjQwNCwiaXNzIjoiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tIiwiYXVkIjpbImh0dHBzOi8vc2NpbS5leGFtcGxlLmNvbS9GZWVkcy85OGQ1MjQ2MWZhNWJiYzg3OTU5M2I3NzU0IiwiaHR0cHM6Ly9zY2ltLmV4YW1wbGUuY29tL0ZlZWRzLzVkNzYwNDUxNmIxZDA4NjQxZDc2NzZlZTciXSwiZXZlbnRzIjp7InVybjppZXRmOnBhcmFtczpzY2ltOmV2ZW50OmNyZWF0ZSI6eyJyZWYiOiJodHRwczovL3NjaW0uZXhhbXBsZS5jb20vVXNlcnMvNDRmNjE0MmRmOTZiZDZhYjYxZTc1MjFkOSIsImF0dHJpYnV0ZXMiOlsiaWQiLCJuYW1lIiwidXNlck5hbWUiLCJwYXNzd29yZCIsImVtYWlscyJdfX19.", policy, out var jwt);
+            Assert.True(result);
+
+            jwt.Payload.TryGetClaim(SecEventClaims.Events, out var events);
+            Assert.Equal("https://scim.example.com/Users/44f6142df96bd6ab61e7521d9", events["urn:ietf:params:scim:event:create"]["ref"].GetString());
+            Assert.True(events.ContainsKey("urn:ietf:params:scim:event:create"));
+            Assert.True(events["urn:ietf:params:scim:event:create"].ContainsKey("ref"));
+            Assert.False(events.ContainsKey("X"));
+            Assert.False(events["urn:ietf:params:scim:event:create"].ContainsKey("Y"));
+        }
+    }
+}
