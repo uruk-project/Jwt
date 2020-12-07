d76a72c770ced4328206c5259864c811133fd243
Yann CRUMEYROLLE <ycrumeyrolle@free.fr>
2020-12-07 23:53:08 +0100

    Fix missing using statement

--- src/JsonWebToken/Cryptography/AesCbcEncryptor.cs
+++ src/JsonWebToken/Cryptography/AesCbcEncryptor.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE in the project root for license information.
 
 using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 
 namespace JsonWebToken.Cryptography
