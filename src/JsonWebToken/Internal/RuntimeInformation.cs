16716dd1057c59394ee98bea356cb76563832496
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2021-02-14 21:58:15 +0100

    Re-enable MacOS  (#535)
    Removed tests with AES-GCM for MacOS

---
+++ src/JsonWebToken/Internal/RuntimeInformation.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2020 Yann Crumeyrolle. All rights reserved.
+// Licensed under the MIT license. See LICENSE in the project root for license information.
+
+#if NET47 || NET461
+namespace JsonWebToken
+{
+    internal class RuntimeInformation
+    {
+        // NET47 is only available on Windows
+        internal static bool IsOSPlatform(OSPlatform os)
+            => os == OSPlatform.Windows;
+    }
+}
+#endif
