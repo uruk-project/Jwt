6a7b6e640e133d9b272b2499580b9d969a4deaf2
Olivier Spinelli <olivier.spinelli@invenietis.com>
2021-02-15 21:44:00 +0100

    One to two map bug fix. (#544)
    Fixed incorrect One to Two map Add.

---
+++ test/JsonWebToken.Tests/JsonObjectTests.cs
@@ -0,0 +1,33 @@
+﻿using JsonWebToken.Cryptography;
+using Xunit;
+
+namespace JsonWebToken.Tests
+{
+    public class JsonObjectTests
+    {
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [Theory]
+        public void AddReplacesExistingMember(int initialMemberCount)
+        {
+            JsonObject o = new JsonObject();
+            for( int i = 0; i<initialMemberCount;++i)
+            {
+                o.Add(i.ToString(), "Padding");
+            }
+            o.Add("A", true);
+            Assert.True(o.TryGetValue("A", out var vT));
+            Assert.True((bool)vT.Value);
+            Assert.Equal(initialMemberCount + 1, o.Count);
+
+            o.Add("A", false);
+            Assert.True(o.TryGetValue("A", out var vF) && vF.Value.Equals(false));
+            Assert.Equal(initialMemberCount + 1, o.Count);
+        }
+    }
+}
