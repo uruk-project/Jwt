27559a0f72bf8d0cdb41b1c84ecff5095b2aaba7
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2021-03-29 17:17:55 +0200

    Update KeyWrapper_AesGcmKeyWrapperTests.cs

--- test/JsonWebToken.Tests/KeyWrapper_AesGcmKeyWrapperTests.cs
+++ test/JsonWebToken.Tests/KeyWrapper_AesGcmKeyWrapperTests.cs
@@ -32,11 +32,6 @@ namespace JsonWebToken.Tests
         [MemberData(nameof(GetAesWrappingAlgorithms))]
         public void TryWrapKey_WithStaticKey_Success(EncryptionAlgorithm enc, KeyManagementAlgorithm alg)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return;
-            }
-
             var contentEncryptionKey = SymmetricJwk.GenerateKey(enc.RequiredKeySizeInBits);
             Jwk cek = TryWrapKey_Success(contentEncryptionKey, enc, alg);
             Assert.Equal(contentEncryptionKey, cek);
@@ -46,11 +41,6 @@ namespace JsonWebToken.Tests
         [MemberData(nameof(GetAesWrappingAlgorithms))]
         public void TryWrapKey_WithoutStaticKey_Success(EncryptionAlgorithm enc, KeyManagementAlgorithm alg)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return;
-            }
-
             Jwk cek = TryWrapKey_Success(null, enc, alg);
             Assert.NotNull(cek);
         }
@@ -58,11 +48,6 @@ namespace JsonWebToken.Tests
         [Fact]
         public void WrapKey_Failure()
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                return;
-            }
-
             var keyEncryptionKey = SymmetricJwk.GenerateKey(256);
             var contentEncryptionKey = SymmetricJwk.GenerateKey(256);
             var wrapper = new AesGcmKeyWrapper(keyEncryptionKey, EncryptionAlgorithm.A256Gcm, KeyManagementAlgorithm.A256GcmKW);
