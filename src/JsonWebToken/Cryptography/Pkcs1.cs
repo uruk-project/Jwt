a085094b7e3e6d20641903edc5e6197f404e9558
ycrumeyrolle <ycrumeyrolle@free.fr>
2024-07-21 19:55:40 +0200

    fix

--- src/JsonWebToken/Cryptography/Pkcs1.cs
+++ src/JsonWebToken/Cryptography/Pkcs1.cs
@@ -11,7 +11,7 @@ namespace JsonWebToken.Cryptography
         public static ReadOnlySpan<char> PrivatRsaKeySuffix => new[] { '-', '-', '-', '-', '-', 'E', 'N', 'D', ' ', 'R', 'S', 'A', ' ', 'P', 'R', 'I', 'V', 'A', 'T', 'E', ' ', 'K', 'E', 'Y', '-', '-', '-', '-', '-' };
 
 #if SUPPORT_ELLIPTIC_CURVE
-#if NET6_0
+#if NET7_0
         public static ReadOnlySpan<char> PrivateECKeyPrefix => "-----BEGIN EC PRIVATE KEY-----".AsSpan();
 #else
         public static ReadOnlySpan<char> PrivateECKeyPrefix => new[] { '-', '-', '-', '-', '-', 'B', 'E', 'G', 'I', 'N', ' ', 'E', 'C', ' ', 'P', 'R', 'I', 'V', 'A', 'T', 'E', ' ', 'K', 'E', 'Y', '-', '-', '-', '-', '-' };
